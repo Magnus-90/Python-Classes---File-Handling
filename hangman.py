4012c23ff3576f186d8b46f68652e6e281f41ba9
Magnus-90 <magnus.hansen@optimatik.ch>
2026-01-16 13:53:04 +0100

    Refactor hangman game logic to improve input handling and display formatting

--- hangman.py
+++ hangman.py
@@ -8,11 +8,12 @@ def startmenu():
     maingame()
 
 def maingame():
+    guessed_letters = []
     i = 0
     wordlength_userinput = 0
     inputs = 0
     word = random.choice(random_words_list) # Zufällige Auswahl
-    word_display = ["_ " for _ in word]
+    word_display = ["_" for _ in word]
     wordlength = len(word)
     max_attempts = len(word) + 2
     print("Das Spiel beginnt")
@@ -21,16 +22,27 @@ def maingame():
     print(" ".join(word_display))
     while wordlength_userinput < wordlength and inputs < max_attempts:
         userinput = input("Geben sie einen Buchstaben ein: ").lower()
+        if userinput in guessed_letters:
+            print(f"{userinput} hast du schon mal eingegeben")
+            continue
+        guessed_letters.append(userinput)
+        inputs = inputs + 1
         if userinput in word:
+            anzahl = word.count(userinput)
             for index, letter in enumerate(word):
                 if letter == userinput:
                     word_display[index] = userinput
-            print("Buchstabe ist im Wort")
-            wordlength_userinput = wordlength_userinput + 1
-            inputs = inputs + 1
+                    wordlength_userinput = wordlength_userinput + 1
+                    print("Buchstabe ist im Wort")
         else:
             inputs = inputs + 1
             print("Buchstabe war nicht im Wort")
+        print(" ".join(word_display))
+        print(f"Versuche: {inputs}/{max_attempts}")
+        print("")
 
-
+    if wordlength_userinput == wordlength:
+        print("🎉 Gewonnen!")
+    else:
+        print("❌ Verloren! Das Wort war:", word)
 startmenu()
