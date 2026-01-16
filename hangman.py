728dcba8e84337e97389d6f602978f8e28c38a02
Magnus-90 <magnus.hansen@optimatik.ch>
2026-01-16 14:51:34 +0100

    Add game continuation option and import sys for exit functionality

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 random_words_list = ["python", "java", "javascript", "typescript", "html", "swift", "ruby"]
 
@@ -44,4 +45,13 @@ def maingame():
         print("Gewonnen!")
     else:
         print("Verloren! Das Wort war:", word)
+    
+    userinput_play_again = input("Möchten sie weiter spielen? (y/n): ").lower()
+    while userinput_play_again != "y" and userinput_play_again != "n":
+        print("Eingabe Ungültig. Bitte geben sie y oder n ein.")
+    match userinput_play_again:
+        case "y":
+            maingame()
+        case "n":
+            sys.exit("Spiel wird beendet...")
 startmenu()
